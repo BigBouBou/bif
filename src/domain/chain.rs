590e50766a8fe91b1c160b0923d33515839e6e15
BigBouBou <boulet.sebas@gmail.com>
2026-04-03 16:50:40 -0400

    methode split_frontmatter_and_body

--- src/domain/chain.rs
+++ src/domain/chain.rs
@@ -1,3 +1,6 @@
 // A note chain is a sequence of notes that are linked together
 // The app points to the current note in the chain
-pub struct Chain {}
+pub struct Chain {
+    pub current_note: PathBuf,
+    pub notes: Vec<Note>,
+}
